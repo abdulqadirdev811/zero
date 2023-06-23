733674dd5255898222d96033e3825a6d3968d7c5
abdulqadirdev811 <abdul.qadir@bluesoftsolutions.com>
2023-06-23 15:38:45 +0500

    relevent files

--- pythonProgramming/python_functions_and_datatypes/sort_the_list_of_sets/sort_list_of_sets.py
+++
@@ -1,8 +0,0 @@
-def sort_multidimentional_array(list_of_dictionary):
-    return sorted(list_of_dictionary,key = lambda x : x[0])
-
-
-def change_list_element_datatype(input_list,data_type):
-    resultant_array = [data_type(i) for i in input_list]
-    return resultant_array
-
